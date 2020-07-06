ae6e824e6643644b7910ef28712be9ad1ad779e1
Bethsheba Zebata <bethshebazebata@BethshesMacBook.attlocal.net>
2020-07-06 14:55:44 -0500

    adding api connections

--- frontend/src/store/Store.js
+++ frontend/src/store/Store.js
@@ -1,9 +1,21 @@
 import { observable, action, computed } from 'mobx';
+import apiInstance from '../utils/api';
 
 class Store {
+    constructor({ api }) {
+        this.api=api
+    }
     @observable likesCount = 7
 
     @observable messages = ["Team", "Everything"]
+    @observable user = {
+        "id": 0,
+        "name": "",
+        "email": "",
+        "token": "",
+        "location": "",
+        "school": ""
+    }
 
     @action updateCount() {
         this.likesCount++;
@@ -17,8 +29,17 @@ class Store {
         return this.messages.length;
     }
 
+    @action login(data) {
+        this.api.login(data).then(results => {
+            console.log(results)
+            this.user=results
+        })
+    }
+
+
 }
 
-const storeInstance = new Store()
 
+const storeInstance = new Store({ api: apiInstance })
+window.storeInstance = storeInstance //this is just for testing. Do not use in source code.
 export default storeInstance;
