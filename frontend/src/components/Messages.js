e37db9ce1d6d51aad53a273753e14f671fc71237
Sondos <sondosissa18@gmail.com>
2020-07-08 19:52:10 -0500

    modified the login form

--- frontend/src/components/Messages.js
+++ frontend/src/components/Messages.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import { useStore } from '../store/useStore';
-import { useObserver } from 'mobx-react';
-
+import React from "react";
+import { useStore } from "../store/useStore";
+import { useObserver } from "mobx-react";
 
 export default function Messages() {
-    const store = useStore()
-    return useObserver(() => (
-        <table className="table">
-            <tbody>
-                {
-                    store.messages.map((message, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>
-                                    {message}
-                                </td>
-                            </tr>
-                        )
-
-                    })
-                }
-            </tbody>
-        </table>
-    )
-    )
+  const store = useStore();
+  return useObserver(() => (
+    <table className="table">
+      <tbody>
+        {store.messageStore.messages.map((message, index) => {
+          return (
+            <tr key={index}>
+              <td>{message}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  ));
 }
