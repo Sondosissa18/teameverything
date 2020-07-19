82d8c4e57abf4fff0f276a435da321bc725620f3
Ralph Hernandez <hernandezralph.1121@gmail.com>
2020-07-18 17:52:14 -0700

    pulling from master

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -33,33 +33,41 @@ export default function Home() {
     <div id="home">
       <h2>EverythingSports </h2>
       <p>The Facebook of Sports.</p>
-      <Container style={{ margin: "0px", padding: "2px" }}>
-        <Row>
-          <Col xs={6} md={3}>
-            <Image
-              src={
-                store.user.picLocation
-                  ? process.env.REACT_APP_API_URL + store.user.picLocation
-                  : "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-              }
-              thumbnail
-            />
-          </Col>
-        </Row>
-      </Container>
-      <h3>{store.user.displayName || "Welcome!!! Click On Profile & Add Your Info!"}</h3>
-      <h6>Bio: {store.user.about} </h6>
-      <h6> School: {store.user.school}</h6>
-      <h6> Location: {store.user.location}</h6>
-
-      <div className="message">
+      <div className="row">
+        <Container style={{ margin: "0px", padding: "2px" }}>
+          <Row>
+            <Col xs={6} md={3}>
+              <Image
+                src={
+                  store.user.picLocation
+                    ? process.env.REACT_APP_API_URL + store.user.picLocation
+                    : "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+                }
+                thumbnail
+              />
+            </Col>
+          </Row>
+        </Container>
+        <h3>
+          {store.user.displayName ||
+            "Welcome!!! Click On Profile & Add Your Info!"}
+        </h3>
+        <h6>Bio: {store.user.about} </h6>
+        <h6> School: {store.user.school}</h6>
+        <h6> Location: {store.user.location}</h6>
+      </div>
+      <div className="row">
+        <h3>Messages</h3>
+        <Message />
+      </div>
+      {/* <div className="message">
         <Container>
           <Col>
             <h3>Messages</h3>
             <Message />
           </Col>
         </Container>
-      </div>
+      </div> */}
     </div>
   ));
 }
