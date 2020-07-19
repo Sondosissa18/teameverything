96d4a2e1c828af5427dc29209ca07041d06be57e
Bethsheba Zebata <bethzebata@gmail.com>
2020-07-19 01:03:26 -0500

    removed blog and messages

--- frontend/src/components/Messages.js
+++ frontend/src/components/Messages.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { useStore } from "../store/useStore";
 import { useObserver } from "mobx-react";
+import { Button } from "react-bootstrap";
 
 export default function Messages() {
   const store = useStore();
   return useObserver(() => (
     <table className="table">
       <tbody>
-        {store.messageStore.messages.map((message, index) => {
+        {store.messageStore.messages.map((message) => {
           return (
-            <tr key={index}>
+            <tr key={message._id}>
               <td>{message}</td>
             </tr>
           );
